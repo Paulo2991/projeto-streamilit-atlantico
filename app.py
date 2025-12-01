168546700a4279ba73b98eb7716d1618eb23c19a
Paulo2991 <paulovaz38034@gmail.com>
2025-12-01 13:03:23 -0300

    Ajuste de projeto inicial

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ if arquivo is not None:
          df = pd.read_csv(arquivo, sep=";")
          st.sidebar.success("Arquivo CSV carregado com sucesso!")
      elif fileExtension in ['.xlsx','.xls']:
-        df = pd.read_excel(arquivo,usecols=[9, 11, 15,25, 26])
+        df = pd.read_excel(arquivo,usecols=[6,7,9,11,12,15,16,18,21,25,26])
         st.sidebar.success("Arquivo Excel carregado com sucesso!")
      else:
          st.sidebar.error("Arquivo não suportado")
@@ -55,10 +55,6 @@ def explodeResponsaveis(df):
     return df.drop(columns=["lista"])  
 
 
-if st.sidebar.button("🔁 Analises Reabertas"):
-    st.session_state.activate_analysis = "reabertas"
-
-
 ##Cards"""
 
 col1, col2,col3 = st.columns(3)
@@ -77,7 +73,10 @@ with col3: card("Maior Data", maior.strftime("%d/%m/%Y") if pd.notnull(maior) el
 relatorios = [
     "Percentual De Tarefas Reabertas",
     "Tarefas Por Responsável",
-    "SLA"
+    "SLA Sem Outliers",
+    "SLA Com Outliers",
+    "Tempo De Soma Por Tipo De Tarefa",
+    "Tempo Medio Por Tipo De Tarefa"
 ]
 
 selecoes = {}
@@ -94,39 +93,162 @@ comoboSelecionado = st.sidebar.selectbox(
 st.sidebar.divider() 
 
     
-if comoboSelecionado == "Percentual De Tarefas Reabertas":
- st.subheader("🔁 Analise De Tarefas Reabertas") 
+def filtroTipoTarefas():
+  filtroTipoTarefa = sorted(df['Tipo de tarefa'].dropna().unique().tolist())
+  filtroTipoTarefaSelectBox = st.sidebar.selectbox("Filtrar Por Tipo De Tarefa",["(todas)"] + filtroTipoTarefa)
+  colunasExibir = ["ID da Tarefa", "Tarefa", "Tipo de tarefa", "Já registradas h"]
+  colunasMostrar = [coluna for coluna in colunasExibir if coluna in df.columns]
+  if filtroTipoTarefaSelectBox == "(todas)":
+    tipoTarefa = df[colunasMostrar].copy()
+    tipoTarefa = tipoTarefa.sort_values("Já registradas h", ascending=False)
+  else:
+    tipoTarefa = df[df["Tipo de tarefa"] == filtroTipoTarefaSelectBox].copy()
+    tipoTarefa = tipoTarefa[colunasMostrar].sort_values("Já registradas h", ascending=False)
+    
+  return tipoTarefa 
 
- count = df["Reaberta?"].value_counts().reset_index()
- count.columns = ["Reaberta","Total"]
+match comoboSelecionado:   
+    case "Percentual De Tarefas Reabertas":
+        st.subheader("🔁 Analise De Tarefas Reabertas") 
+        dfReabertadasSim = explodeResponsaveis(df[df["Reaberta?"] == "Sim"])
 
- fig = px.pie(count, names="Reaberta", values="Total", hole=0.4)
- st.plotly_chart(fig,use_container_width=True)
-elif comoboSelecionado == "Tarefas Por Responsável":
-  dfReabertadasSim = explodeResponsaveis(df[df["Reaberta?"] == "Sim"])
+        count = df["Reaberta?"].value_counts().reset_index()
+        count.columns = ["Reaberta","Total"]
+
+        fig = px.pie(count, names="Reaberta", values="Total", hole=0.4)
+        st.plotly_chart(fig,use_container_width=True)
+        
+        pessoa = sorted(dfReabertadasSim['Tipo de tarefa'].dropna().unique())
+        pessoaSelectBox = st.sidebar.selectbox("Filtrar Por Resposável",["(todas)"] + pessoa)
+        criarSelectBox = dfReabertadasSim if pessoaSelectBox == "(todas)" else dfReabertadasSim[dfReabertadasSim["Responsável"] == pessoaSelectBox]
   
-  dfResponsavel = dfReabertadasSim.groupby("Responsável").size().reset_index(name="Total").sort_values("Total")
+        st.dataframe(
+            criarSelectBox[
+                ["Tipo de tarefa", "Responsável", "Criada em", "Reaberta?"]
+            ].sort_values("Criada em", ascending=False),
+            use_container_width=True
+        )
+    case "Tarefas Por Responsável":
+        dfReabertadasSim = explodeResponsaveis(df[df["Reaberta?"] == "Sim"])
+  
+        dfResponsavel = dfReabertadasSim.groupby("Responsável").size().reset_index(name="Total").sort_values("Total")
  
-  graficoReabertoResponsavel = px.bar(
-    dfResponsavel,
-    x="Total", y="Responsável",
-    orientation="h", text="Total", height=800
-     
- )
+        graficoReabertoResponsavel = px.bar(
+            dfResponsavel,
+            x="Total", y="Responsável",
+            orientation="h", text="Total", height=800
+        )
  
-  graficoReabertoResponsavel.update_layout( xaxis=dict(showgrid=True, gridcolor="#cccccc", gridwidth=0.5),
+        graficoReabertoResponsavel.update_layout( xaxis=dict(showgrid=True, gridcolor="#cccccc", gridwidth=0.5),
         yaxis=dict(showgrid=False),)
-  st.plotly_chart(graficoReabertoResponsavel, use_container_width=True)
+        st.plotly_chart(graficoReabertoResponsavel, use_container_width=True)
   
-  pessoa = sorted(dfReabertadasSim['Responsável'].unique())
-  pessoaSelectBox = st.sidebar.selectbox("Filtrar Por Resposável",["(todas)"] + pessoa)
-  criarSelectBox = dfReabertadasSim if pessoaSelectBox == "(todas)" else dfReabertadasSim[dfReabertadasSim["Responsável"] == pessoaSelectBox]
+        pessoa = sorted(dfReabertadasSim['Responsável'].dropna().unique())
+        pessoaSelectBox = st.sidebar.selectbox("Filtrar Por Resposável",["(todas)"] + pessoa)
+        criarSelectBox = dfReabertadasSim if pessoaSelectBox == "(todas)" else dfReabertadasSim[dfReabertadasSim["Responsável"] == pessoaSelectBox]
   
-  st.dataframe(
-        criarSelectBox[
-            ["Tarefa", "Responsável", "Criada em", "Reaberta?"]
-        ].sort_values("Criada em", ascending=False),
-        use_container_width=True
+        st.dataframe(
+            criarSelectBox[
+                ["Tarefa", "Responsável", "Criada em", "Reaberta?"]
+            ].sort_values("Criada em", ascending=False),
+            use_container_width=True
+        )
+  
+    case "SLA Sem Outliers":
+        st.subheader(":blue[📈 Análise SLA]")
+    ##----------------Informe os dados que estão vazios-------------------------
+        df["Entrega desejada"] = df["Entrega desejada"].fillna("Não Informado")
+        df["Fechada em"] = df["Fechada em"].fillna("Não Informado")
+        df['Entrega desejada'] = pd.to_datetime(df['Entrega desejada'], errors='coerce')
+        df['Fechada em'] = pd.to_datetime(df['Fechada em'], errors='coerce')
+    ##--- Faz o calculo da diferencia entre a entrega fechada e a desejada
+        df['diferencaHoras'] = (df['Fechada em'] - df['Entrega desejada']) / pd.Timedelta(days=1) * 24 * -1
+        Q1 = df["diferencaHoras"].quantile(0.25)
+        Q3 = df["diferencaHoras"].quantile(0.75)
+        IQR = Q3 - Q1
+        limiteInferior = Q1 - 1.5 * IQR
+        limiteSuperior = Q3 + 1.5 * IQR
+        diferencaSemOutlires = df[(df["diferencaHoras"] >= limiteInferior) & (df["diferencaHoras"] <= limiteSuperior)]
+        dfSlaTarefaMedio = diferencaSemOutlires.groupby("Tipo de tarefa")["diferencaHoras"].mean().reset_index().sort_values("diferencaHoras",ascending=True)
+
+    
+        fig1 = px.bar(dfSlaTarefaMedio,
+                x='diferencaHoras',
+                y='Tipo de tarefa',
+                orientation='h',
+                color='Tipo de tarefa',
+                color_continuous_scale='RdYlGn',
+                labels={'diferenciaHoras': 'Média da Diferença de Horas (SLA)', 'Tipo de tarefa': 'Tipo de Tarefa'},
+                title='Tempo Médio (SLA) entre Entrega Desejada e Fechada por Tipo de Tarefa (Sem Outliers)',
+                height=900) 
+
+    # Ajusta o layout para melhor legibilidade
+        fig1.update_layout(
+            title_font_size=20,
+            xaxis_title_font_size=14,
+            yaxis_title_font_size=14,
+            xaxis_tickfont_size=10,
+            yaxis_tickfont_size=10,
+            yaxis_categoryorder='total ascending' # Garante a ordem das barras
     )
+
+        st.plotly_chart(fig1, use_container_width=True)
+    
+    ## -----Tempo Medio Por Tipo De Tarefa
+    case "Tempo De Soma Por Tipo De Tarefa":
+     st.markdown("## 🧮 Análise de Tempo Total e Soma por Tipo de Tarefa")
+     dfFechado = df[df['Fechada em'].notna()]
+     tempoTotalTarefa = (
+         dfFechado.groupby("Tipo de tarefa")["Já registradas h"]
+         .sum()
+         .reset_index()
+         .sort_values("Já registradas h",ascending=False)
+    )
+     st.markdown("### 📊 Tempo Total Registrado por Tarefa")
+     tempoTotalTarefa["Já registradas h"] = tempoTotalTarefa["Já registradas h"].round(2)
+     figTempoTotalTarefa = px.bar(
+         tempoTotalTarefa,
+         x='Tipo de tarefa',
+         y='Já registradas h',
+         color='Tipo de tarefa',
+         color_continuous_scale='RdYlGn',
+         labels={'Já registradas h': 'Tempo Total', 'Tipo de tarefa': 'Tipo de Tarefa'},
+         height=500)
+     figTempoTotalTarefa.update_traces(textposition="outside")
+     figTempoTotalTarefa.update_layout(xaxis_tickangle=-45)
+     st.plotly_chart(figTempoTotalTarefa, use_container_width=True)
+     
+     st.dataframe(
+            filtroTipoTarefas(),
+            use_container_width=True
+        )
   
+    case "Tempo Medio Por Tipo De Tarefa":
+        st.markdown("## 🧮 Análise de Tempo Total e Medio por Tipo de Tarefa")
+        dfFechado = df[df['Fechada em'].notna()]
+        tempoTotalTarefa = (
+         dfFechado.groupby("Tipo de tarefa")["Já registradas h"]
+         .mean()
+         .reset_index()
+         .sort_values("Já registradas h",ascending=False)
+    )
+        tempoTotalTarefa["Já registradas h"] = tempoTotalTarefa["Já registradas h"].round(2)
+        st.markdown("### 📊 Tempo Médio Registrado por Tarefa")
+        figTempoTotalTarefa = px.bar(
+         tempoTotalTarefa,
+         x='Tipo de tarefa',
+         y='Já registradas h',
+         color='Tipo de tarefa',
+         color_continuous_scale='RdYlGn',
+         labels={'Já registradas h': 'Tempo Medio', 'Tipo de tarefa': 'Tipo de Tarefa'},
+         height=500)
+        figTempoTotalTarefa.update_traces(textposition="outside")
+        figTempoTotalTarefa.update_layout(xaxis_tickangle=-45)
+        st.plotly_chart(figTempoTotalTarefa, use_container_width=True)
+        filtro = filtroTipoTarefas()
+        st.dataframe(
+            filtro,
+            use_container_width=True
+        )
+    
   
